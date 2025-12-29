70c8dd6c29440ca2ac45bd9440e2b4c385a8cde7
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 22:00:33 +0300

    Update ArenaLogs.js

--- models/ArenaLogs.js
+++ models/ArenaLogs.js
@@ -1,1 +1,18 @@
 
+const mongoose = require('mongoose');
+
+const arenaLogSchema = new mongoose.Schema({
+    challenger: { type: String, required: true }, // Meydan okuyan
+    opponent: { type: String, required: true },   // Rakip
+    winner: String,
+    rounds: [{
+        roundNumber: Number,
+        attacker: String,
+        damage: Number,
+        remainingHP: Number
+    }],
+    totalPrize: Number,
+    date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('ArenaLog', arenaLogSchema);
