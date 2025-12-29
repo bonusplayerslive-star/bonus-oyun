5a92d9d19b740e16b4de742d9b4c6923f0786945
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 07:00:34 +0300

    Victory.js

--- models/Victory.js
+++ models/Victory.js
@@ -1,1 +1,4 @@
 
+const mongoose = require('mongoose');
+const VictorySchema = new mongoose.Schema({ email: String, nickname: String, bpl: Number, date: { type: Date, default: Date.now } });
+module.exports = mongoose.model('Victory', VictorySchema);
