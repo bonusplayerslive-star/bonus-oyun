161ffc3985db75e1ae0271575800a467b1e03443
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 03:48:21 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,15 @@
-// Path: models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    // Temel Bilgiler
     nickname: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-
-    // Ekonomi Sistemi
-    bpl: { type: Number, default: 2500 }, // Yeni kayıtlara 2500 hediye
-    inventory: { type: [String], default: [] }, // Sahip olunan hayvanlar
-
-    // Gelişim ve Savaş İstatistikleri
-    // Örnek yapı: { "Aslan": { hp: 120, atk: 15, def: 10 }, "Kaplan": { ... } }
+    bpl: { type: Number, default: 2500 }, 
+    inventory: { type: [String], default: [] },
     stats: { type: Object, default: {} },
-
-    // Cüzdan Bilgileri
-    bnb_address: { type: String, default: '' }, // Çekim yapılacak adres
-    usdt_address: { type: String, default: '' }, // Yatırım yapılan adres
-
-    // Ödeme ve Güvenlik
-    usedHashes: { type: [String], default: [] } // Daha önce onaylanmış TX Hash kodları
-}, { timestamps: true }); // Kayıt tarihlerini otomatik ekler
+    bnb_address: { type: String, default: '' },
+    usdt_address: { type: String, default: '' },
+    usedHashes: { type: [String], default: [] }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
