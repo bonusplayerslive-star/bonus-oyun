871fabd6120aa7a532510685f7d635fe109e5b97
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 22:00:02 +0300

    Update Punishment.js

--- models/Punishment.js
+++ models/Punishment.js
@@ -1,1 +1,11 @@
 
+const mongoose = require('mongoose');
+
+const punishmentSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    bpl: { type: Number, required: true }, // Kaybedilen miktar
+    reason: { type: String, default: 'Arena Defeat' }, // Ceza sebebi
+    date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Punishment', punishmentSchema);
