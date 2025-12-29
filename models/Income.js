a1cc04c031588f44454d84666443ad78006dba27
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 07:02:30 +0300

    Income.js

--- models/Income.js
+++ models/Income.js
@@ -1,1 +1,4 @@
 
+const mongoose = require('mongoose');
+const IncomeSchema = new mongoose.Schema({ userId: String, nickname: String, amount: Number, roomId: String, date: { type: Date, default: Date.now } });
+module.exports = mongoose.model('Income', IncomeSchema);
