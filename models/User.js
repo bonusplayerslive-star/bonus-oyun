c3160003007a6a06bc3bb7d844fce4e3aa79ba5f
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 03:59:36 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
-
 const UserSchema = new mongoose.Schema({
     nickname: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    bpl: { type: Number, default: 2500 }, 
+    bpl: { type: Number, default: 2500 },
     inventory: { type: [String], default: [] },
     stats: { type: Object, default: {} },
     bnb_address: { type: String, default: '' },
     usdt_address: { type: String, default: '' },
     usedHashes: { type: [String], default: [] }
 }, { timestamps: true });
-
 module.exports = mongoose.model('User', UserSchema);
