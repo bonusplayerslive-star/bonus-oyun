1b2202b91698b02c2ebf9240c564eff7a8b21b3e
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 21:57:43 +0300

    Delete models/Punishment.js

--- models/Punishment.js
+++
@@ -1,4 +0,0 @@
-
-const mongoose = require('mongoose');
-const PunishmentSchema = new mongoose.Schema({ email: String, bpl: Number, reason: String, date: { type: Date, default: Date.now } });
-module.exports = mongoose.model('Punishment', PunishmentSchema);
