d02b482980a0fb43dd0bdf57d5c94e73de745766
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 07:01:25 +0300

    Punishment.js

--- models/Punishment.js
+++ models/Punishment.js
@@ -1,1 +1,4 @@
 
+const mongoose = require('mongoose');
+const PunishmentSchema = new mongoose.Schema({ email: String, bpl: Number, reason: String, date: { type: Date, default: Date.now } });
+module.exports = mongoose.model('Punishment', PunishmentSchema);
