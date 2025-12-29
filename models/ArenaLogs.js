3c818545eb2d7891bfecc5a2862cdd83de46c761
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 05:59:41 +0300

    ArenaLogs.js

--- models/ArenaLogs.js
+++ models/ArenaLogs.js
@@ -1,1 +1,8 @@
 
+const Victory = mongoose.model('Victory', new mongoose.Schema({
+    email: String, nickname: String, bpl: Number, date: { type: Date, default: Date.now }
+}));
+
+const Punishment = mongoose.model('Punishment', new mongoose.Schema({
+    email: String, bpl: Number, reason: String, date: { type: Date, default: Date.now }
+}));
