3f5730f5bcab1b33f500cb84aba6d8404395a889
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-29 21:58:02 +0300

    Delete models/Victory.js

--- models/Victory.js
+++
@@ -1,4 +0,0 @@
-
-const mongoose = require('mongoose');
-const VictorySchema = new mongoose.Schema({ email: String, nickname: String, bpl: Number, date: { type: Date, default: Date.now } });
-module.exports = mongoose.model('Victory', VictorySchema);
