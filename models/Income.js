8302d067639dbdf9eed38e24319d5d151eeba9fb
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-31 00:19:26 +0300

    Update Income.js

--- models/Income.js
+++ models/Income.js
@@ -1,20 +1,15 @@
-
 const mongoose = require('mongoose');
 
 const IncomeSchema = new mongoose.Schema({
-    // UserId'yi doğrudan User modeline bağlıyoruz (Populate yapabilmek için)
-    userId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User', 
-        required: true 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
     nickname: { type: String, required: true },
     amount: { type: Number, required: true },
-    
-    // Gelirin kaynağı (Örn: "Arena Win", "Meeting Fee", "Daily Bonus")
-    source: { type: String, default: 'Arena Win' }, 
-    
-    roomId: { type: String }, // Eğer bir savaştan veya masadan geliyorsa
+    source: { type: String, default: "Arena Win" }, // Arena Win, Meeting Fee, Daily Bonus
+    roomId: { type: String },
     date: { type: Date, default: Date.now }
 });
 
