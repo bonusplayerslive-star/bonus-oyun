a9a579bef5ea016c2260255210b1cf0a980a2f05
bonusplayerslive-star <bonusplayerslive@gmail.com>
2025-12-31 00:19:39 +0300

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
-    userId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User', 
-        required: true 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
     nickname: { type: String, required: true },
-    txid: { 
-        type: String, 
-        unique: true, 
+    txid: {
+        type: String,
+        unique: true,
         required: true,
-        trim: true // Boşluk hatalarını engeller
+        trim: true
     },
     amountUSD: { type: Number, required: true },
     amountBPL: { type: Number, required: true },
-    network: { type: String, default: 'BEP20' }, // Örn: BEP20 (BNB Chain)
-    status: { 
-        type: String, 
-        enum: ['pending', 'completed', 'failed'], 
-        default: 'pending' 
+    network: { type: String, default: 'BEP20' },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
     },
     date: { type: Date, default: Date.now }
 });
