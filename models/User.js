e34cc86ebec5374670f29fa6a47e0209cac56f12
bonusplayerslive-star <bonusplayerslive@gmail.com>
2026-01-01 15:01:17 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     nickname: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true }, // Email eklendi
     password: { type: String, required: true },
-    bpl: { type: Number, default: 1000 },
+    bpl: { type: Number, default: 2500 }, // Başlangıç bonusu 2500
     usdt_address: { type: String, default: '' },
     inventory: [{
         name: String,
         level: { type: Number, default: 1 },
         stats: {
-            hp: { type: Number, default: 100 },
-            atk: { type: Number, default: 20 },
+            hp: { type: Number, default: 150 },
+            atk: { type: Number, default: 30 },
             def: { type: Number, default: 10 }
         }
     }],
