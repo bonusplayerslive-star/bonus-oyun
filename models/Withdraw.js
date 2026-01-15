7d8667b4327bf90fc48c86463e6fa0f9bc6f396b
bonusplayerslive-star <bonusplayerslive@gmail.com>
2026-01-15 04:31:53 +0300

    Update Withdraw.js

--- models/Withdraw.js
+++ models/Withdraw.js
@@ -1,1 +1,16 @@
 
+const mongoose = require('mongoose');
+
+const WithdrawSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    nickname: String,
+    email: String,
+    requestedAmount: Number, // Çekmek istediği miktar
+    commission: Number,      // %25 Kesinti
+    finalAmount: Number,     // Ele geçecek net miktar
+    walletAddress: String,   // Gönderilecek Metamask adresi
+    status: { type: String, default: 'Beklemede' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Withdraw', WithdrawSchema);
