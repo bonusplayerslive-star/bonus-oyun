9e5afa733f038e2a5f62c21732548df5deaf9ad5
bonusplayerslive-star <bonusplayerslive@gmail.com>
2026-01-15 04:54:16 +0300

    Update Help.js

--- models/Help.js
+++ models/Help.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const HelpSchema = new mongoose.Schema({
     email: { type: String, required: true },
-    nickname: { type: String, required: true },
+    nickname: { type: String, required: false },
     subject: { type: String, required: true }, // "Talep İptali" veya "Genel Destek"
     message: { type: String, required: true },
     status: { type: String, default: 'Beklemede' }, // Beklemede, İncelendi, Tamamlandı
