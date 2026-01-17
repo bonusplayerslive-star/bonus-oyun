0204a63083adb70a090e805a0b2bd4a8398cc4d7
bonusplayerslive-star <bonusplayerslive@gmail.com>
2026-01-18 00:00:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const path = require('path');
 require('dotenv').config();
 
@@ -163,3 +163,4 @@ io.on('connection', async (socket) => {
 // --- 5. SERVER BAŞLAT ---
 const PORT = process.env.PORT || 10000;
 httpServer.listen(PORT, () => console.log(`🌍 Sunucu Yayında: ${PORT}`));
+
