3724a6310d8e60e0aae57d66f7829d3b66c898de
bonusplayerslive-star <bonusplayerslive@gmail.com>
2026-01-18 09:42:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo').default;
+const MongoStore = require('connect-mongo');
 const path = require('path');
 require('dotenv').config();
 
@@ -243,5 +243,6 @@ async function createBotMatch(p) {
 // --- 6. SERVER START ---
 const PORT = process.env.PORT || 10000;
 httpServer.listen(PORT, () => console.log(`🌍 BPL Sunucu Hazır: ${PORT}`));
+
 
 
